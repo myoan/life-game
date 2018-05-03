98ad1f22e281bb47a016c6778ab658a5ad4c38f8
myoan <motoki.yoan@gmail.com>
2018-05-03 15:38:38 +0900

    Fix Layout/TrailingWhitespace

--- lib/life_game/board.rb
+++ lib/life_game/board.rb
@@ -3,14 +3,14 @@ module LifeGame
     attr_accessor :w, :h, :cells
     def initialize(w, h, default = {})
       @w = w
-      @h = h 
+      @h = h
       @cells = initialize_cells(default[:pattern])
     end
 
     def finish?
       cells.all?(&:dead?)
     end
-  
+
     def initialize_cells(default_pattern = [])
       default_pattern = Array.new(w * h) { false } if default_pattern.nil? || default_pattern.empty?
       Array.new(w * h) do |i|
@@ -19,7 +19,7 @@ module LifeGame
         Cell.new(x, y, default_pattern[i])
       end
     end
-  
+
     def get_cell(x, y)
       return nil if x < 0
       return nil if y < 0
@@ -27,7 +27,7 @@ module LifeGame
       return nil if y > h
       cells[coordinate_to_index(x, y)]
     end
-  
+
     def surround(cell)
       result = []
       x = cell.x
@@ -42,7 +42,7 @@ module LifeGame
       result << get_cell(x + 1, y + 1)
       result.flatten.compact
     end
-  
+
     def surround_alive(cell)
       surround(cell).count { |c| c&.alive? }
     end
@@ -54,7 +54,7 @@ module LifeGame
     def display
       cells.map(&:show).join
     end
-  
+
     def step
       new_cells = initialize_cells
       cells.each_with_index do |cell, i|
