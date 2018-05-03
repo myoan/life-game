64acb53f6a96807940ff7b43b9c1b29361ba43b8
myoan <motoki.yoan@gmail.com>
2018-05-03 15:25:11 +0900

    Fix Style/RedundantBegin

--- lib/life_game.rb
+++ lib/life_game.rb
@@ -4,18 +4,16 @@ require_relative './life_game/window_manager'
 
 module LifeGame
   def self.run(x, y, opt)
-    begin
-      b = Board.new(x, y, opt)
-      win = WindowManager.new(x, y)
-      loop do
-        break if b.finish?
-        win.exec(b.display)
-        b.step
-        sleep(0.2)
-      end
-    ensure
-      win.close
+    b = Board.new(x, y, opt)
+    win = WindowManager.new(x, y)
+    loop do
+      break if b.finish?
+      win.exec(b.display)
+      b.step
+      sleep(0.2)
     end
+  ensure
+    win.close
   end
 end
 
